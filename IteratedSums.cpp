64ec241cefb19c83783597a5dde45d7371f2002f
rajdeepbharati <30660439+rajdeepbharati@users.noreply.github.com>
2018-06-15 15:14:50 +0530

    Update IteratedSums.cpp

--- IteratedSums.cpp
+++ IteratedSums.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-	int a, b;
-	cin >> a >> b;
-	cout << (b*(b+1)*(2*b+1)/6)-((a-1)*a*(2*a-1)/6) << endl;
-	return 0;
+    int a, b;
+    cin >> a >> b;
+    cout << (b * (b + 1) * (2 * b + 1) / 6) - ((a - 1) * a * (2 * a - 1) / 6) << endl;
+    return 0;
 }
