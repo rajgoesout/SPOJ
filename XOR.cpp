40bfdb64afc94531d17deac5bed7231025cccb44
rajdeepbharati <30660439+rajdeepbharati@users.noreply.github.com>
2018-06-15 15:19:04 +0530

    Create XOR.cpp

---
+++ XOR.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (x == y)
+        cout << 0 << endl;
+    else
+        cout << 1 << endl;
+    return 0;
+}
