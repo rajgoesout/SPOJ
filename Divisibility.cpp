4cd556195055b1c43d621ceb0375008f0f659147
rajdeepbharati <30660439+rajdeepbharati@users.noreply.github.com>
2018-06-15 15:13:39 +0530

    Create Divisibility.cpp

---
+++ Divisibility.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int n, x, y;
+    for (int i = 1; i <= t; i++) {
+        cin >> n >> x >> y;
+        for (int j = 2; j < n; j++) {
+            if (j % x == 0 && j % y != 0) {
+                cout << j << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
