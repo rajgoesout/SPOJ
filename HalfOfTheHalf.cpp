6fe62c1e6fa4ef0f3a880bea404876dd40bcd289
rajdeepbharati <30660439+rajdeepbharati@users.noreply.github.com>
2018-06-15 15:23:53 +0530

    Create HalfOfTheHalf.cpp

---
+++ HalfOfTheHalf.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int t;
+    char s[200];
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        cin >> s;
+        int len = strlen(s);
+        for (int j = 0; j < len / 2; j += 2) {
+            cout << s[j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
