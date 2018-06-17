5d19d32ac58ed711c517b71779964efdfa58e723
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-06-17 16:14:02 +0530

    Update WillItBlend.cpp

--- WillItBlend.cpp
+++ WillItBlend.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 int main() {
-	int t;
-	cin >> t;
-	long n, k;
-	for (int i = 1; i <= t; i++) {
-		cin >> n >> k;
-		long ni;
-		for (long j = 1; j <= n; j++){
-			cin >> ni;
-			if (k > ni)
-				cout << "Yes, it blends!" << endl;
-			else
-				cout << "Well, it mostly blended" << endl;
-		}
-	}
-	return 0;
+    int t;
+    cin >> t;
+    long n, k;
+    for (int i = 1; i <= t; i++) {
+        cin >> n >> k;
+        long ni;
+        for (long j = 1; j <= n; j++) {
+            cin >> ni;
+            if (k > ni)
+                cout << "Yes, it blends!" << endl;
+            else
+                cout << "Well, it mostly blended" << endl;
+        }
+    }
+    return 0;
 }
