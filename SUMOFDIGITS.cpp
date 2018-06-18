7947be7bc25888dd30512aae3f42e7fd81fc4d18
Mohammad Amaan <amaanabbasi99@gmail.com>
2018-06-18 11:21:45 +0530

    smaller code

--- SUMOFDIGITS.cpp
+++ SUMOFDIGITS.cpp
@@ -5,45 +5,18 @@ using namespace std;
 int main() {
     int t;
     cin >> t;
-    char s[100];
+    int s;
+    int last;
     for (int i = 1; i <= t; i++) {
         cin >> s;
         int j = 0, sum = 0;
-        for (j = 0; s[j] != '\0'; j++) {
-            switch (s[j]) {
-                case '1':
-                    sum++;
-                    break;
-                case '2':
-                    sum += 2;
-                    break;
-                case '3':
-                    sum += 3;
-                    break;
-                case '4':
-                    sum += 4;
-                    break;
-                case '5':
-                    sum += 5;
-                    break;
-                case '6':
-                    sum += 6;
-                    break;
-                case '7':
-                    sum += 7;
-                    break;
-                case '8':
-                    sum += 8;
-                    break;
-                case '9':
-                    sum += 9;
-                    break;
-                default:
-                    sum += 0;
-                    break;
-            }
-        }
+        	while(s!='\0'){
+        		last =  s % 10;
+				s = s/10;
+				sum += last;
+			}
+        
         cout << sum << endl;
-    }
+}
     return 0;
 }
