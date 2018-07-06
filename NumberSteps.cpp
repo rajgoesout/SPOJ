4c238a68f08fc6d869874c7b3584cb1a7b681210
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-06 17:58:01 +0530

    Create NumberSteps.cpp

---
+++ NumberSteps.cpp
@@ -0,0 +1,27 @@
+// https://www.spoj.com/problems/NSTEPS/
+#include <iostream>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	int x, y;
+	for (int i = 1; i <= t; i++){
+		cin >> x >> y;
+		if (x==y){
+			if (x%2==0){
+				cout << x*2 << endl;
+			}else{
+				cout << x*2 - 1 << endl;
+			}
+		}else if (x == y+2){
+			if (x%2==0){
+				cout << (x-1)*2 << endl;
+			}else{
+				cout << (x-1)*2 - 1 << endl;
+			}
+		}else{
+			cout << "No Number" << endl;
+		}
+	}
+}
