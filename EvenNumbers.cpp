1aa892c1f9f92e9906213467655ad6e6ffc70ba0
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-08 13:53:55 +0530

    Update EvenNumbers.cpp

--- EvenNumbers.cpp
+++ EvenNumbers.cpp
@@ -2,32 +2,31 @@
 #include <iostream>
 using namespace std;
 
-unsigned long reverseBits(unsigned long n)
-{
+unsigned long reverseBits(unsigned long n) {
     unsigned long rev = 0;
-     
-    while (n > 0)
-    {
+
+    while (n > 0) {
         rev <<= 1;
-         
+
         if (n & 1 == 1)
             rev ^= 1;
-        n >>= 1;    
+        n >>= 1;
     }
-     
+
     return rev;
 }
 
-int main(){
-	int n;
-	cin >> n;
-	long x;
-	for (int i = 1; i <= n; i++){
-		cin >> x;
-		if (x%2!=0){
-			cout << x << endl;
-		}else{
-			cout << reverseBits(x) << endl;
-		}
-	}
+int main() {
+    int n;
+    cin >> n;
+    long x;
+    for (int i = 1; i <= n; i++) {
+        cin >> x;
+        if (x % 2 != 0) {
+            cout << x << endl;
+        } else {
+            cout << reverseBits(x) << endl;
+        }
+    }
+    return 0;
 }
