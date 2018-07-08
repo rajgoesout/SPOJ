63abe7f7db5bb8953bd53d2cb36d489914ae6e1e
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-08 13:56:16 +0530

    Update NumberSteps.cpp

--- NumberSteps.cpp
+++ NumberSteps.cpp
@@ -2,26 +2,27 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int t;
-	cin >> t;
-	int x, y;
-	for (int i = 1; i <= t; i++){
-		cin >> x >> y;
-		if (x==y){
-			if (x%2==0){
-				cout << x*2 << endl;
-			}else{
-				cout << x*2 - 1 << endl;
-			}
-		}else if (x == y+2){
-			if (x%2==0){
-				cout << (x-1)*2 << endl;
-			}else{
-				cout << (x-1)*2 - 1 << endl;
-			}
-		}else{
-			cout << "No Number" << endl;
-		}
-	}
+int main() {
+    int t;
+    cin >> t;
+    int x, y;
+    for (int i = 1; i <= t; i++) {
+        cin >> x >> y;
+        if (x == y) {
+            if (x % 2 == 0) {
+                cout << x * 2 << endl;
+            } else {
+                cout << x * 2 - 1 << endl;
+            }
+        } else if (x == y + 2) {
+            if (x % 2 == 0) {
+                cout << (x - 1) * 2 << endl;
+            } else {
+                cout << (x - 1) * 2 - 1 << endl;
+            }
+        } else {
+            cout << "No Number" << endl;
+        }
+    }
+    return 0;
 }
