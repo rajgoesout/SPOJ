70e884f523e2668aaea007fea87cba3cbab2ca63
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-08 13:56:48 +0530

    Update SUMOFDIGITS.cpp

--- SUMOFDIGITS.cpp
+++ SUMOFDIGITS.cpp
@@ -10,11 +10,11 @@ int main() {
     for (int i = 1; i <= t; i++) {
         cin >> s;
         int j = 0, sum = 0;
-	while (s != '\0') {
-	    last =  s % 10;
-	    s = s/10;
-	    sum += last;
-	}
+        while (s != '\0') {
+            last = s % 10;
+            s = s / 10;
+            sum += last;
+        }
         cout << sum << endl;
     }
     return 0;
