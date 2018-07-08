57c0776ece12f3f9716e2575f07093a1d5468a5e
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-08 13:53:11 +0530

    Update ADDREV.cpp

--- ADDREV.cpp
+++ ADDREV.cpp
@@ -2,23 +2,24 @@
 #include <iostream>
 using namespace std;
 
-int reverse(int a){
-	int rev=0;
-	while(a>0){
-		rev = rev*10 + a%10;
-		a/=10;
-	}
-	return rev;
+int reverse(int a) {
+    int rev = 0;
+    while (a > 0) {
+        rev = rev * 10 + a % 10;
+        a /= 10;
+    }
+    return rev;
 }
 
-int main(){
-	int n;
-	cin >> n;
-	int x, y;
-	for (int i=1; i<=n; i++){
-		cin >> x >> y;
-		x=reverse(x);
-		y=reverse(y);
-		cout << reverse(x+y) << endl;
-	}
+int main() {
+    int n;
+    cin >> n;
+    int x, y;
+    for (int i = 1; i <= n; i++) {
+        cin >> x >> y;
+        x = reverse(x);
+        y = reverse(y);
+        cout << reverse(x + y) << endl;
+    }
+    return 0;
 }
