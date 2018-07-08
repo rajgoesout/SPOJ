e3aa0e19c4df9e1070965e65f841076cbe44d055
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-08 13:55:26 +0530

    Update Hangover.cpp

--- Hangover.cpp
+++ Hangover.cpp
@@ -2,19 +2,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	float c, sum;
-	int n;
-	while(true){
-		cin >> c;
-		if (c==0.00)
-		break;
-		sum=0.0;
-		n=1;
-		while(sum<c){
-			sum += float(1/float(n+1));
-			n++;
-		}
-		cout << n-1 << " card(s)" << endl;
-	};
+int main() {
+    float c, sum;
+    int n;
+    while (true) {
+        cin >> c;
+        if (c == 0.00)
+            break;
+        sum = 0.0;
+        n = 1;
+        while (sum < c) {
+            sum += float(1 / float(n + 1));
+            n++;
+        }
+        cout << n - 1 << " card(s)" << endl;
+    }
+    return 0;
 }
