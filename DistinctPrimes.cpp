f54b911478e0711fbd280dbd5f38b7b577c7b92e
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-19 16:52:28 +0530

    Update DistinctPrimes.cpp

--- DistinctPrimes.cpp
+++ DistinctPrimes.cpp
@@ -4,42 +4,6 @@ using namespace std;
 
 const int MAX = 3000;
 
-void RevMerge(int a[], int low, int high, int mid) {
-    int i = low, j = mid + 1, k = 0;
-    int temp[high - low + 1];
-
-    while (i <= mid && j <= high) {
-        if (a[i] > a[j])
-            temp[k++] = a[i++];
-        else
-            temp[k++] = a[j++];
-    }
-
-    while (i <= mid) {
-        temp[k++] = a[i++];
-    }
-
-    while (j <= high) {
-        temp[k++] = a[j++];
-    }
-
-    for (i = low; i <= high; i++) {
-        a[i] = temp[i - low];
-    }
-}
-
-
-void RevMergeSort(int a[], int low, int high) {
-    int mid;
-    if (low < high) {
-        mid = (low + high) / 2;
-        RevMergeSort(a, low, mid);
-        RevMergeSort(a, mid + 1, high);
-
-        RevMerge(a, low, high, mid);
-    }
-}
-
 void merge(int arr[], int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
@@ -107,7 +71,6 @@ int smartNumber(int n) {
         }
     }
     mergeSort(result, 0, k - 1);
-
     return result[n - 1];
 }
 
